e4aa4efce7aa4906f00d4ad25a4ddc2719c9b491
Dave Lim <dlimiter@gmail.com>
2011-06-30 15:32:11 +1000

    Tweaks to remove some minor distracting non-content

--- theage.com.au.js
+++ theage.com.au.js
@@ -1,5 +1,18 @@
-
+/*
+Ads
+*/
 $('.ad').remove();
 $('.fN-affiliateStrip').remove();
 $('.c5.classifieds.cfix').remove();
+
+/*
+Flashy distracty non-content
+*/
+// RHS Video news and promotions 
+$('.cT-videoNews').remove();
+$('.cT-featuredNews').remove();
+$('.cT-quoteRotator').remove();
+
+
+
 
