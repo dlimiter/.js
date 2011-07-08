b9a6522483dfbbbf0334773e8727ddb750f4f16a
Dave Lim <dlimiter@gmail.com>
2011-07-08 13:30:15 +1000

    Added a rule to remove the big advertising sidebars

--- theage.com.au.js
+++ theage.com.au.js
@@ -4,14 +4,18 @@ Ads
 $('.ad').remove();
 $('.fN-affiliateStrip').remove();
 $('.c5.classifieds.cfix').remove();
+// Big background ad
+$("a:contains('Advertisement')").remove();
 
 /*
 Flashy distracty non-content
 */
-// RHS Video news and promotions 
+// RHS Video news and promotions
 $('.cT-videoNews').remove();
 $('.cT-featuredNews').remove();
 $('.cT-quoteRotator').remove();
+
+
 
 
 
