ac195a2720d31831fa8b6cf3683a8cce55cbfc04
Dave Lim <dlimiter@gmail.com>
2011-11-07 13:36:48 +1100

    Corrected my lolsyntax

--- heraldsun.com.au.js
+++ heraldsun.com.au.js
@@ -6,6 +6,4 @@ Make the herals sun bearable for viewing
 Step 1. Remove objectionable content
 Step 2. Insert Kitteh
 */
-$('body').replaceWith('<body><h1 style="display:block;font-family:sans-serif;text-align:center; width:700px;margin:10px auto;">JAVASCRIPT KITTEH WIL SAVE U FROM URSELFS. NO HERULD SUN FUR U! </h1><img style="display:block;margin-left:auto;margin-right:auto" alt="kitteh" src="http://i1205.photobucket.com/albums/bb438/LKC55/a110e187.jpg"></body>');
-
-
+$('body').replaceWith('<body><h1 style="display:block;font-family:sans-serif;text-align:center; width:700px;margin:10px auto;">JAVASCRIPT KITTEH WILL SAVE U FRUM YOURSELVEZ. NO HERALD SUN 4 U!</h1><img style="display:block;margin-left:auto;margin-right:auto" alt="kitteh" src="http://i1205.photobucket.com/albums/bb438/LKC55/a110e187.jpg"></body>');
